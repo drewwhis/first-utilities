e901534a6bada5e19b2f1391dad236b5cc173a12
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-04-27 21:50:51 -0500

    add unit tests

---
+++ FIRST.Utilities/FIRST.Utilities.Test/Services/FtcApiServiceTests.cs
@@ -0,0 +1,54 @@
+using System.Net.Http.Headers;
+using System.Text;
+using FIRST.Utilities.Options;
+using FIRST.Utilities.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+
+namespace FIRST.Utilities.Test.Services;
+
+public class FtcApiServiceTests
+{
+    private IHttpClientFactory GetMockFactory()
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddUserSecrets<FtcApiOptions>()
+            .Build();
+
+        var username = configuration.GetValue<string>("FtcApi:Username")!;
+        var password = configuration.GetValue<string>("FtcApi:Password")!;
+        
+        var basicAuthenticationValue = Convert.ToBase64String(
+            Encoding.ASCII.GetBytes(
+                $"{username}:{password}"
+            )
+        );
+        
+        var mockFactory = new Mock<IHttpClientFactory>();
+        var client = new HttpClient
+        {
+            BaseAddress = new Uri("https://ftc-api.firstinspires.org/v2.0/")
+        };
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", basicAuthenticationValue);
+        mockFactory.Setup(f => f.CreateClient(It.IsAny<string>())).Returns(client);
+        return mockFactory.Object;
+    }
+
+    private IOptions<FtcApiOptions> GetMockOptions()
+    {
+        var mockOptions = new Mock<IOptions<FtcApiOptions>>();
+        return mockOptions.Object;
+    }
+    
+    [Fact]
+    public async Task Test_CanConnect()
+    {
+        var factory = GetMockFactory();
+        var settings = GetMockOptions();
+        var service = new FtcApiService(settings, factory);
+
+        var result = await service.CanConnect();
+        Assert.True(result);
+    }
+}
