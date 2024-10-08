0a6adcaaa1f6a74877a213c5c7b958a5f589cb5a
Drew Whisenant <7390131+drewwhis@users.noreply.github.com>
2024-10-07 19:16:53 -0500

    fix warnings

--- FIRST.Utilities.Models/TeamLocation.cs
+++ FIRST.Utilities.Models/TeamLocation.cs
@@ -3,6 +3,6 @@ namespace FIRST.Utilities.Models;
 public class TeamLocation
 {
     public int TeamNumber { get; set; }
-    public string TeamName { get; set; }
+    public required string TeamName { get; set; }
     public TeamLocationType LocationType { get; set; }
 }
